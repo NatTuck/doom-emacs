e9e913c709b4b2c7683137caa0865db9e10692a1
Nat Tuck <nat@ferrus.net>
2019-04-14 18:42:25 -0400

    Add my configs to git.

---
+++ modules/private/nattuck/config.el
@@ -0,0 +1,6 @@
+;;; private/nattuck/config.el -*- lexical-binding: t; -*-
+
+(setq doom-font (font-spec :family "Hack" :size 22)
+      doom-variable-pitch-font (font-spec :family "DejaVu Sans")
+      doom-unicode-font (font-spec :family "DejaVu Sans Mono")
+      doom-big-font (font-spec :family "Hack" :size 28))
